72f2a4d9600a7bc9d87c40e3862c93ee939f8d0b
Aman Dekate <amandekate007@gmail.com.com>
2024-02-05 11:53:10 +0530

    Added  Mobile responsiveness

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faYoutube, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const SocialMediaLinks = () => {
     return (
@@ -11,6 +11,9 @@ const SocialMediaLinks = () => {
             
             <a href="https://www.linkedin.com/in/aman-dekate-1b8254195/" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a href="https://twitter.com/aman_dekate" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faXTwitter} size="2x" />
             </a>
         </div>
     );
